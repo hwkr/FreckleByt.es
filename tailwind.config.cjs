3dfad956143ba05f9c5b0184639cb63057d4e740
Brynn Hawker <brynn@hawker.me>
2022-08-28 18:41:50 -0700

    Font again

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,8 +5,14 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			colors: {
+				pink: {
+					100: "#ffd9e0",
+					500: "#fe4365",
+				}
+			},
 			fontFamily: {
-				sans: ['Source Sans 3', ...defaultTheme.fontFamily.sans] 
+				sans: ["'Source Sans 3Variable'", ...defaultTheme.fontFamily.sans] 
 			},
 		},
 	},
